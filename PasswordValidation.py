90c789f0540712e99ae5afc1936f8d44498aefdb
Bharat Maheshwari <59102335+BharatMaheshwari96@users.noreply.github.com>
2020-01-26 13:32:00 +0000

    Update PasswordValidation.py

--- PasswordValidation.py
+++ PasswordValidation.py
@@ -3,3 +3,10 @@ def InputPassword(password):
     pas = input("Enter the passwords(Comma separated):")
     password = pas.split(',')
     return password
+
+
+#Execution Starts Here
+password = []
+password = InputPassword(password)
+ProcessPassword(password)
+
